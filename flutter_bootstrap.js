0a46c04d2664b6f05c94ee617de9329d8947ce7c
anugerah170405 <anugerahgari170405@gmail.com>
2024-12-19 13:55:44 +0800

    add new files to replace the old

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2592335380"
+    serviceWorkerVersion: "2209037832"
   }
 });
